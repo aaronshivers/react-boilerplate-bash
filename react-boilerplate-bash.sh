4fd6be12a3ebd92cc929ba4eb2f6cdec64e68542
aaron shivers <aaorn@moonmail.org>
2019-07-21 23:48:28 -0500

    rearranged stylesheet links

--- react-boilerplate-bash.sh
+++ react-boilerplate-bash.sh
@@ -124,13 +124,13 @@ echo "<!DOCTYPE html>
 <html>
 <head>
   <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-  <link rel=\"stylesheet\" type=\"text/css\" href=\"/dist/styles.css\">
   <link
     rel=\"stylesheet\"
     href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\"
     integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\"
     crossorigin=\"anonymous\"
   />
+  <link rel=\"stylesheet\" type=\"text/css\" href=\"/dist/styles.css\">
   <title>$1</title>
 </head>
 <body>
